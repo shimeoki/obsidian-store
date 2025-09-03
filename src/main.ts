731e75a3560f53529b4696d631e73e5a23669a88
shimeoki <shimeoki@gmail.com>
2025-09-03 22:15:29 +0300

    feat: test code to check the plugin load

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-// todo: source here
+import { Plugin } from "obsidian";
+
+export default class Store extends Plugin {
+	async onload(): Promise<void> {
+		console.log("generating a random uuid:");
+		console.log(crypto.randomUUID());
+	}
+
+	async onunload(): Promise<void> {
+		console.log("unloading");
+	}
+}
