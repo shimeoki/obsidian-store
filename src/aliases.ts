654aed1cfaad054a38d0b496de058fc8ac9fbc6d
shimeoki <shimeoki@gmail.com>
2025-09-08 15:47:55 +0300

    feat: add test command

--- src/aliases.ts
+++ src/aliases.ts
@@ -10,18 +10,19 @@ export default class Aliases {
 
     constructor(plugin: Store) {
         this.plugin = plugin
+        this.addCommands()
     }
 
-    public async add(file: TFile) {
+    private aliases(file: TFile): string[] {
         const app = this.plugin.app
 
         if (!isMarkdown(file)) {
-            return
+            return []
         }
 
         const meta = app.metadataCache.getFileCache(file)
-        if (meta == null) {
-            return
+        if (!meta) {
+            return []
         }
 
         const headings = meta.headings || []
@@ -29,16 +30,45 @@ export default class Aliases {
 
         const titles = headings
             .filter((h) => h.level == 1)
-            .map((h) => h.heading.trim())
+            .map((h) => h.heading)
             .filter((h) => h != "" && !aliases.some((a) => a == h))
 
         if (titles.length == 0) {
+            return []
+        }
+
+        return aliases.concat(titles)
+    }
+
+    public async add(file: TFile) {
+        const aliases = this.aliases(file).filter((a) => a) // non-null
+        if (aliases.length == 0) {
             return
         }
 
-        aliases.push(...titles)
-        await app.fileManager.processFrontMatter(file, (fm) => {
-            fm.aliases = aliases
+        await this.plugin.app.fileManager.processFrontMatter(
+            file,
+            (fm) => fm.aliases = aliases,
+        )
+    }
+
+    private addCommands() {
+        const plugin = this.plugin
+        plugin.addCommand({
+            id: "store-add-aliases",
+            name: "Add aliases",
+            checkCallback: (checking) => {
+                const file = plugin.app.workspace.getActiveFile()
+                if (!file || this.aliases(file).length == 0) {
+                    return false
+                }
+
+                if (!checking) {
+                    this.add(file)
+                }
+
+                return true
+            },
         })
     }
 }
