b11ff0684eb4171b7a832280915e33b77605edc7
shimeoki <shimeoki@gmail.com>
2025-09-08 09:14:21 +0300

    refactor: suggest in separate file
    and rename FileSuggest to NoteSuggest

--- src/settings/tab.ts
+++ src/settings/tab.ts
@@ -1,13 +1,6 @@
 import Store from "@/main.ts"
-import {
-    AbstractInputSuggest,
-    App,
-    PluginSettingTab,
-    Setting,
-    TAbstractFile,
-    TFile,
-    TFolder,
-} from "obsidian"
+import { App, PluginSettingTab, Setting } from "obsidian"
+import { FolderSuggest, NoteSuggest } from "@/settings/suggest.ts"
 
 export default class StoreSettingTab extends PluginSettingTab {
     plugin: Store
@@ -53,51 +46,7 @@ export default class StoreSettingTab extends PluginSettingTab {
                         await this.plugin.setTemplate(path)
                     )
 
-                new FileSuggest(this.app, text.inputEl)
+                new NoteSuggest(this.app, text.inputEl)
             })
-    }
-}
-
-abstract class PathSuggest<T extends TAbstractFile>
-    extends AbstractInputSuggest<T> {
-    protected abstract items(): T[]
-
-    protected override getSuggestions(query: string): T[] | Promise<T[]> {
-        const items = this.items()
-        const match = query.toLowerCase()
-        return items.filter((item) => item.path.toLowerCase().includes(match))
-    }
-
-    override renderSuggestion(value: T, el: HTMLElement) {
-        const query = this.getValue().toLowerCase()
-        const path = value.path
-
-        const index = path.toLowerCase().indexOf(query)
-        if (index == -1) {
-            el.createSpan({ text: path })
-            return
-        }
-
-        el.createSpan({ text: path.substring(0, index) })
-        el.createEl("b", { text: path.substring(index, index + query.length) })
-        el.createSpan({ text: path.substring(index + query.length) })
-    }
-
-    override selectSuggestion(value: T, _evt: MouseEvent | KeyboardEvent) {
-        this.setValue(value.path)
-        this.textInputEl.trigger("input") // note: unofficial api
-        this.close()
-    }
-}
-
-class FileSuggest extends PathSuggest<TFile> {
-    protected override items(): TFile[] {
-        return this.app.vault.getMarkdownFiles()
-    }
-}
-
-class FolderSuggest extends PathSuggest<TFolder> {
-    protected override items(): TFolder[] {
-        return this.app.vault.getAllFolders(true)
     }
 }
