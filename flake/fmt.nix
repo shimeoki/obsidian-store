fc6dcbcd8eb798f3a802930d1dddbb510f6b30ff
shimeoki <shimeoki@gmail.com>
2025-11-20 17:10:26 +0300

    feat(flake): separate treefmt config
    for nix and toml versions in the root

--- flake/fmt.nix
+++ flake/fmt.nix
@@ -5,36 +5,6 @@
     ];
 
     perSystem = {
-        treefmt = {
-            programs = {
-                # keep-sorted start block=yes newline_separated=yes
-                deno = {
-                    enable = true;
-                    includes = [
-                        "src/"
-                        "deno.json"
-                        "manifest.json"
-                        "package.json"
-                        "tsconfig.json"
-                        "vite.config.ts"
-                    ];
-                };
-
-                keep-sorted = {
-                    enable = true;
-                };
-
-                nixfmt = {
-                    enable = true;
-                    width = 80;
-                };
-                # keep-sorted end
-            };
-
-            settings.formatter = {
-                # TODO: use indent option after numtide/treefmt-nix#416
-                nixfmt.options = [ "--indent=4" ];
-            };
-        };
+        treefmt = import "${inputs.self}/treefmt.nix";
     };
 }
