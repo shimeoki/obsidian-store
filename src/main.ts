5bd5644b56fc65b7c65306bd8688f3f2e16f5277
shimeoki <shimeoki@gmail.com>
2026-02-13 12:10:32 +0300

    refactor: merge archiver into main class

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
-import { normalizePath, Plugin, SplitDirection, TFile, TFolder } from "obsidian"
+import {
+    normalizePath,
+    Notice,
+    Plugin,
+    SplitDirection,
+    TFile,
+    TFolder,
+} from "obsidian"
 
 import Heading from "@/heading.ts"
-import Archiver from "@/archiver.ts"
 import SettingTab from "@/tab.ts"
 
 import Translation from "@/i18n.ts"
@@ -24,7 +30,6 @@ export default class Store extends Plugin {
         this.addRibbonActions()
 
         new Heading(this)
-        new Archiver(this)
     }
 
     override async onunload() {
@@ -157,6 +162,23 @@ export default class Store extends Plugin {
                 return true
             },
         })
+
+        this.addCommand({
+            id: "archive-active",
+            name: l10n.archiveActive.name,
+            checkCallback: (checking) => {
+                const file = this.app.workspace.getActiveFile()
+                if (!file || !this.hasArchiveTag(this.getTags(file))) {
+                    return false
+                }
+
+                if (!checking) {
+                    this.archiveNote(file)
+                }
+
+                return true
+            },
+        })
     }
 
     private addMenus() {
@@ -211,6 +233,53 @@ export default class Store extends Plugin {
                         .setIcon("package")
                         .onClick(async () => await this.pack(file))
                 })
+            }),
+        )
+
+        this.registerEvent(
+            this.app.workspace.on("file-menu", (menu, af) => {
+                if (af instanceof TFolder) {
+                    menu.addItem((item) => {
+                        item
+                            .setTitle(l10n.archiveNotes.title)
+                            .setIcon("folder-archive")
+                            .onClick(async () => {
+                                const notice = new Notice(
+                                    l10n.archiveNotes.folder(af.path),
+                                    0,
+                                )
+
+                                const count = await this.archiveNotes(af)
+
+                                if (count > 0) {
+                                    notice.setMessage(
+                                        l10n.archiveNotes.count(count),
+                                    )
+                                } else {
+                                    notice.setMessage(
+                                        l10n.archiveNotes.empty,
+                                    )
+                                }
+
+                                setTimeout(() => notice.hide(), 3000)
+                            })
+                    })
+
+                    return
+                }
+
+                if (af instanceof TFile) {
+                    if (!this.hasArchiveTag(this.getTags(af))) {
+                        return
+                    }
+
+                    menu.addItem((item) => {
+                        item
+                            .setTitle(l10n.archiveNote.title)
+                            .setIcon("file-archive")
+                            .onClick(() => this.archiveNote(af))
+                    })
+                }
             }),
         )
     }
@@ -401,6 +470,74 @@ export default class Store extends Plugin {
 
     private async pack(file: TFile) {
         await this.copy(this.getAllLinkedFiles(file))
+    }
+
+    private async getArchiveFolder(): Promise<TFolder> {
+        const vault = this.app.vault
+        const path = this.settings.archive.folder
+
+        const folder = vault.getFolderByPath(path)
+        if (folder != null) {
+            return folder
+        }
+
+        await vault.createFolder(path)
+        return await this.getArchiveFolder()
+    }
+
+    // TODO: what if no cache?
+    private getTags(f: TFile) {
+        const cache = this.app.metadataCache
+
+        const meta = cache.getFileCache(f)
+        if (meta) {
+            return meta.frontmatter?.tags || []
+        }
+
+        return []
+    }
+
+    private hasArchiveTag(tags: string[]) {
+        const tag = this.settings.archive.tag
+        return !!tags.find((t) => t == tag)
+    }
+
+    private async archiveNote(f: TFile) {
+        if (!this.hasArchiveTag(this.getTags(f))) {
+            return false
+        }
+
+        const name = `${uuid()}.${f.extension}`
+        const folder = await this.getArchiveFolder()
+
+        const fm = this.app.fileManager
+        await fm.renameFile(f, normalizePath(`${folder.path}/${name}`))
+
+        return true
+    }
+
+    private async archiveNotes(f: TFolder) {
+        const queue = [f]
+        let count = 0
+
+        while (queue.length > 0) {
+            const q = queue.shift()
+            if (!q) {
+                break
+            }
+
+            for (const af of q.children) {
+                if (af instanceof TFolder) {
+                    queue.push(af)
+                } else if (af instanceof TFile) {
+                    if (await this.archiveNote(af)) {
+                        count++
+                    }
+                }
+            }
+        }
+
+        return count
     }
 }
 
