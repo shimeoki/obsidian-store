a275bb59d74b070ac835b21651051bdabae8bc06
shimeoki <shimeoki@gmail.com>
2026-02-13 18:24:30 +0300

    refactor: named folders
    also move commands, menus and ribbon actions to the bottom

--- src/main.ts
+++ src/main.ts
@@ -121,17 +121,31 @@ export default class Store extends Plugin {
         await this.saveData(normalize(this.settings))
     }
 
-    public async getFolder(): Promise<TFolder> {
+    private folderPath(name: Folder): string {
+        const folders = this.settings.folders
+        switch (name) {
+            case "notes":
+                return folders.notes
+            case "assets":
+                return folders.assets
+            case "archive":
+                return folders.archive
+            case "pack":
+                return folders.pack
+        }
+    }
+
+    public async getFolder(name: Folder): Promise<TFolder> {
         const vault = this.app.vault
-        const path = this.settings.folders.notes
+        const path = this.folderPath(name)
 
         const folder = vault.getFolderByPath(path)
-        if (folder != null) {
+        if (folder) {
             return folder
         }
 
         await vault.createFolder(path)
-        return await this.getFolder()
+        return vault.getFolderByPath(path)!
     }
 
     public async readTemplate(): Promise<string> {
@@ -149,240 +163,6 @@ export default class Store extends Plugin {
         }
 
         return await vault.cachedRead(file)
-    }
-
-    private addCommands() {
-        const l10n = this.translation.commands
-
-        this.addCommand({
-            id: "create-vertical-split",
-            name: l10n.createVerticalSplit.name,
-            callback: async () => await this.createSplit("vertical"),
-        })
-
-        this.addCommand({
-            id: "create-horizontal-split",
-            name: l10n.createHorizontalSplit.name,
-            callback: async () => await this.createSplit("horizontal"),
-        })
-
-        this.addCommand({
-            id: "create-tab",
-            name: l10n.createTab.name,
-            callback: async () => await this.createTab(),
-        })
-
-        this.addCommand({
-            id: "move-active",
-            name: l10n.moveActive.name,
-            checkCallback: (checking) => {
-                // otherwise shows if no file is open.
-                // editorCheckCallback doesn't work, because
-                // opened images don't count as "in editor"
-                if (this.app.workspace.getActiveFile() == null) {
-                    return false
-                }
-
-                if (this.activeInStore()) {
-                    return false
-                }
-
-                if (!checking) {
-                    this.moveActive()
-                }
-
-                return true
-            },
-        })
-
-        this.addCommand({
-            id: "add-aliases",
-            name: l10n.addAliasesActive.name,
-            checkCallback: (checking) => {
-                const file = this.app.workspace.getActiveFile()
-                if (!file || this.getAliases(file).length == 0) {
-                    return false
-                }
-
-                if (!checking) {
-                    this.addAliases(file)
-                }
-
-                return true
-            },
-        })
-
-        this.addCommand({
-            id: "pack-active",
-            name: l10n.packActive.name,
-            checkCallback: (checking) => {
-                const file = this.app.workspace.getActiveFile()
-                if (!file) {
-                    return false
-                }
-
-                if (!checking) {
-                    this.pack(file)
-                }
-
-                return true
-            },
-        })
-
-        this.addCommand({
-            id: "archive-active",
-            name: l10n.archiveActive.name,
-            checkCallback: (checking) => {
-                const file = this.app.workspace.getActiveFile()
-                if (!file || !this.hasArchiveTag(this.getTags(file))) {
-                    return false
-                }
-
-                if (!checking) {
-                    this.archiveNote(file)
-                }
-
-                return true
-            },
-        })
-
-        this.addCommand({
-            id: "add-heading",
-            name: l10n.addHeadingActive.name,
-            checkCallback: (checking) => {
-                const file = this.app.workspace.getActiveFile()
-                if (!file || !this.headingProcessor(file)) {
-                    return false
-                }
-
-                if (!checking) {
-                    this.addHeading(file)
-                }
-
-                return true
-            },
-        })
-    }
-
-    private addAbstractFileMenu(cb: (menu: Menu, file: TAbstractFile) => any) {
-        this.registerEvent(this.app.workspace.on("file-menu", cb))
-    }
-
-    private addFileMenu(cb: (menu: Menu, file: TFile) => any) {
-        this.addAbstractFileMenu((menu, file) => {
-            if (file instanceof TFile) {
-                cb(menu, file)
-            }
-        })
-    }
-
-    private addFolderMenu(cb: (menu: Menu, file: TFolder) => any) {
-        this.addAbstractFileMenu((menu, file) => {
-            if (file instanceof TFolder) {
-                cb(menu, file)
-            }
-        })
-    }
-
-    private addMenuItem(menu: Menu, data: MenuItemData) {
-        menu.addItem((item) =>
-            item
-                .setTitle(data.title)
-                .setIcon(data.icon)
-                .onClick(data.cb)
-        )
-    }
-
-    private addMenus() {
-        const l10n = this.translation.menus
-
-        this.addFileMenu((menu, file) => {
-            if (this.inStore(file.path)) {
-                return
-            }
-
-            this.addMenuItem(menu, {
-                title: l10n.move.title,
-                icon: "folder-input",
-                cb: async () => this.move(file.path),
-            })
-        })
-
-        this.addFileMenu((menu, file) => {
-            if (this.getAliases(file).length == 0) {
-                return
-            }
-
-            this.addMenuItem(menu, {
-                title: l10n.addAliases.title,
-                icon: "forward",
-                cb: async () => this.addAliases(file),
-            })
-        })
-
-        this.addFileMenu((menu, file) => {
-            this.addMenuItem(menu, {
-                title: l10n.pack.title,
-                icon: "package",
-                cb: async () => this.pack(file),
-            })
-        })
-
-        this.addFileMenu((menu, file) => {
-            if (!this.headingProcessor(file)) {
-                return
-            }
-
-            this.addMenuItem(menu, {
-                title: l10n.addHeading.title,
-                icon: "heading-1",
-                cb: async () => this.addHeading(file),
-            })
-        })
-
-        this.addFileMenu((menu, file) => {
-            if (!this.hasArchiveTag(this.getTags(file))) {
-                return
-            }
-
-            this.addMenuItem(menu, {
-                title: l10n.archiveNote.title,
-                icon: "file-archive",
-                cb: async () => this.archiveNote(file),
-            })
-        })
-
-        this.addFolderMenu((menu, file) => {
-            this.addMenuItem(menu, {
-                title: l10n.archiveNotes.title,
-                icon: "folder-archive",
-                cb: async () => {
-                    const notice = new Notice(
-                        l10n.archiveNotes.folder(file.path),
-                        0,
-                    )
-
-                    const count = await this.archiveNotes(file)
-                    if (count > 0) {
-                        notice.setMessage(l10n.archiveNotes.count(count))
-                    } else {
-                        notice.setMessage(l10n.archiveNotes.empty)
-                    }
-
-                    setTimeout(() => notice.hide(), 3000)
-                },
-            })
-        })
-    }
-
-    private addRibbonActions() {
-        const l10n = this.translation.ribbonActions
-
-        this.addRibbonIcon(
-            "folder-pen",
-            l10n.new.title,
-            async () => await this.createTab(),
-        )
     }
 
     public inStore(path: string): boolean {
@@ -414,7 +194,7 @@ export default class Store extends Plugin {
             return
         }
 
-        const store = await this.getFolder()
+        const store = await this.getFolder("notes")
         const newPath = `${store.path}/${uuid()}.${file.extension}`
 
         await this.app.fileManager.renameFile(file, normalizePath(newPath))
@@ -430,7 +210,7 @@ export default class Store extends Plugin {
     }
 
     public async create(): Promise<TFile> {
-        const folder = await this.getFolder()
+        const folder = await this.getFolder("notes")
         const path = normalizePath(`${folder.path}/${uuid()}.md`)
         const template = await this.readTemplate()
         return await this.app.vault.create(path, template)
@@ -674,5 +454,239 @@ export default class Store extends Plugin {
                 const indexes = headings.map((h) => h.position.start.line)
                 return combine(shifter(indexes), inserter(title, offset))
         }
+    }
+
+    private addCommands() {
+        const l10n = this.translation.commands
+
+        this.addCommand({
+            id: "create-vertical-split",
+            name: l10n.createVerticalSplit.name,
+            callback: async () => await this.createSplit("vertical"),
+        })
+
+        this.addCommand({
+            id: "create-horizontal-split",
+            name: l10n.createHorizontalSplit.name,
+            callback: async () => await this.createSplit("horizontal"),
+        })
+
+        this.addCommand({
+            id: "create-tab",
+            name: l10n.createTab.name,
+            callback: async () => await this.createTab(),
+        })
+
+        this.addCommand({
+            id: "move-active",
+            name: l10n.moveActive.name,
+            checkCallback: (checking) => {
+                // otherwise shows if no file is open.
+                // editorCheckCallback doesn't work, because
+                // opened images don't count as "in editor"
+                if (this.app.workspace.getActiveFile() == null) {
+                    return false
+                }
+
+                if (this.activeInStore()) {
+                    return false
+                }
+
+                if (!checking) {
+                    this.moveActive()
+                }
+
+                return true
+            },
+        })
+
+        this.addCommand({
+            id: "add-aliases",
+            name: l10n.addAliasesActive.name,
+            checkCallback: (checking) => {
+                const file = this.app.workspace.getActiveFile()
+                if (!file || this.getAliases(file).length == 0) {
+                    return false
+                }
+
+                if (!checking) {
+                    this.addAliases(file)
+                }
+
+                return true
+            },
+        })
+
+        this.addCommand({
+            id: "pack-active",
+            name: l10n.packActive.name,
+            checkCallback: (checking) => {
+                const file = this.app.workspace.getActiveFile()
+                if (!file) {
+                    return false
+                }
+
+                if (!checking) {
+                    this.pack(file)
+                }
+
+                return true
+            },
+        })
+
+        this.addCommand({
+            id: "archive-active",
+            name: l10n.archiveActive.name,
+            checkCallback: (checking) => {
+                const file = this.app.workspace.getActiveFile()
+                if (!file || !this.hasArchiveTag(this.getTags(file))) {
+                    return false
+                }
+
+                if (!checking) {
+                    this.archiveNote(file)
+                }
+
+                return true
+            },
+        })
+
+        this.addCommand({
+            id: "add-heading",
+            name: l10n.addHeadingActive.name,
+            checkCallback: (checking) => {
+                const file = this.app.workspace.getActiveFile()
+                if (!file || !this.headingProcessor(file)) {
+                    return false
+                }
+
+                if (!checking) {
+                    this.addHeading(file)
+                }
+
+                return true
+            },
+        })
+    }
+
+    private addAbstractFileMenu(cb: (menu: Menu, file: TAbstractFile) => any) {
+        this.registerEvent(this.app.workspace.on("file-menu", cb))
+    }
+
+    private addFileMenu(cb: (menu: Menu, file: TFile) => any) {
+        this.addAbstractFileMenu((menu, file) => {
+            if (file instanceof TFile) {
+                cb(menu, file)
+            }
+        })
+    }
+
+    private addFolderMenu(cb: (menu: Menu, file: TFolder) => any) {
+        this.addAbstractFileMenu((menu, file) => {
+            if (file instanceof TFolder) {
+                cb(menu, file)
+            }
+        })
+    }
+
+    private addMenuItem(menu: Menu, data: MenuItemData) {
+        menu.addItem((item) =>
+            item
+                .setTitle(data.title)
+                .setIcon(data.icon)
+                .onClick(data.cb)
+        )
+    }
+
+    private addMenus() {
+        const l10n = this.translation.menus
+
+        this.addFileMenu((menu, file) => {
+            if (this.inStore(file.path)) {
+                return
+            }
+
+            this.addMenuItem(menu, {
+                title: l10n.move.title,
+                icon: "folder-input",
+                cb: async () => this.move(file.path),
+            })
+        })
+
+        this.addFileMenu((menu, file) => {
+            if (this.getAliases(file).length == 0) {
+                return
+            }
+
+            this.addMenuItem(menu, {
+                title: l10n.addAliases.title,
+                icon: "forward",
+                cb: async () => this.addAliases(file),
+            })
+        })
+
+        this.addFileMenu((menu, file) => {
+            this.addMenuItem(menu, {
+                title: l10n.pack.title,
+                icon: "package",
+                cb: async () => this.pack(file),
+            })
+        })
+
+        this.addFileMenu((menu, file) => {
+            if (!this.headingProcessor(file)) {
+                return
+            }
+
+            this.addMenuItem(menu, {
+                title: l10n.addHeading.title,
+                icon: "heading-1",
+                cb: async () => this.addHeading(file),
+            })
+        })
+
+        this.addFileMenu((menu, file) => {
+            if (!this.hasArchiveTag(this.getTags(file))) {
+                return
+            }
+
+            this.addMenuItem(menu, {
+                title: l10n.archiveNote.title,
+                icon: "file-archive",
+                cb: async () => this.archiveNote(file),
+            })
+        })
+
+        this.addFolderMenu((menu, file) => {
+            this.addMenuItem(menu, {
+                title: l10n.archiveNotes.title,
+                icon: "folder-archive",
+                cb: async () => {
+                    const notice = new Notice(
+                        l10n.archiveNotes.folder(file.path),
+                        0,
+                    )
+
+                    const count = await this.archiveNotes(file)
+                    if (count > 0) {
+                        notice.setMessage(l10n.archiveNotes.count(count))
+                    } else {
+                        notice.setMessage(l10n.archiveNotes.empty)
+                    }
+
+                    setTimeout(() => notice.hide(), 3000)
+                },
+            })
+        })
+    }
+
+    private addRibbonActions() {
+        const l10n = this.translation.ribbonActions
+
+        this.addRibbonIcon(
+            "folder-pen",
+            l10n.new.title,
+            async () => await this.createTab(),
+        )
     }
 }
